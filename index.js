975a18133990eb44f85c34699dfad1a913dcf5a9
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-11 23:12:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
-}
+  const stringToArray = string.split('')
+  const reverseStringToArray = stringToArray.reverse()
+  const reverseString = reverseStringToArray.join('')
+  if(string == reverseString) {
+        return true
+    }
+    else {
+        return false
+    }
+}              
+//testing to confirm
+console.log(isPalindrome('abba'))
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('a'))
+console.log(isPalindrome('robot'))
+console.log(isPalindrome('ab'))
 
 /* 
   Add your pseudocode here
+  //define function isPalindrome(string)
+ convert string into an array:
+       const stringToArray = string.split('');
+ reverse the array:
+       const reverseStringToArray =    stringToArray.reverse();
+ convert array to a string
+       const reverseString =   reverseStringToArray.join('') 
+ if(string == reverseString) {
+        console.log('True');
+    }
+    else {
+        console.log('False');
+    }
+}              
 */
 
 /*
   Add written explanation of your solution here
+   SUMMARY:
+ a function that checks if a word is palindrome.
+ it should take in take in one argument that is a string in lowercase(all letters)
+if the string is palindrome it should return 'true' else 'false'.
+
+TEST CASES:
+When the function is called with the argument ('mom') expect return value is 'true'
+When the function is called with the argument ('clove') expect return value is 'false'
+ SUMMARY:
+ a function that checks if a word is palindrome.
+ it should take in take in one argument that is a string in lowercase(all letters)
+if the string is palindrome it should return 'true' else 'false'.
+
+TEST CASES:
+When the function is called with the argument ('mom') expect return value is 'true'
+When the function is called with the argument ('clove') expect return value is 'false'
+
 */
 
 // You can run `node index.js` to view these console logs
